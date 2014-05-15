162cf5ef92790d4217522c97090bf600f52bd1b5
Wal McConnell <walter.mcconnell@gmail.com>
2014-05-15 17:24:31 +0100

    line chart and box plot

--- analysis.R
+++ analysis.R
@@ -1,24 +1,24 @@
 library(gmodels)
 library(ggplot2)
+library(scales)
 
-data <- read.csv("data/PPR-2014-Dublin_cleaned.csv", stringsAsFactors=FALSE)
-str(data)
+
+data.2013<- read.csv('data/cleaned/PPR-2013-Dublin.csv', stringsAsFactors = FALSE)
+data.2014 <- read.csv('data/cleaned/PPR-2014-Dublin.csv', stringsAsFactors = FALSE)
+
+data <- rbind(data.2013, data.2014)
 
 names(data) <- c('Date.of.Sale','Address','Postal.Code','County','Price','Not.Full.Market.Price','VAT.Exclusive','Description.of.Property','Property.Size.Description')
 
+str(data)
 data$Postal.Code <- as.factor(data$Postal.Code)
 data$County <- as.factor(data$County)
-
+data$Date.of.Sale <- as.Date(data$Date.of.Sale,"%m/%d/%Y")
+data$SaleMonth <- as.Date(cut(data$Date.of.Sale,  breaks = "month"))
 data$Description.of.Property <- as.factor(data$Description.of.Property)
 data$Property.Size.Description <- as.factor(data$Property.Size.Description)
 data$VAT.Exclusive <- as.factor(data$VAT.Exclusive)
 data$Not.Full.Market.Price <- as.factor(data$Not.Full.Market.Price)
-
-str(data)
-
-postcode_by_price_range<- function(postcode, price_range) {
-    subset(data, Postal.Code == postcode & Price %in% price_range)
-}
 
 
 price_range <- 250000:700000
@@ -26,7 +26,9 @@ post_codes <- c('Dublin 3', 'Dublin 5')
 
 data.subset <- subset(data, Price %in% price_range & Postal.Code %in% post_codes)
 
-jpeg("boxplot.jpg")
+pdf("2013-2014-summary.pdf")
+ggplot(data.subset, aes(SaleMonth,Price)) + stat_summary(fun.y = mean, geom="line") + geom_smooth(method='lm')
+
 ggplot(data.subset, aes(Postal.Code, Price)) + geom_boxplot()
 dev.off();
 
--- bin/clean_data.rb
+++ bin/clean_data.rb
@@ -13,15 +13,18 @@ def remove_commas(string)
   string.gsub(',','')
 end
 
-CSV.open('data/PPR-2014-Dublin_cleaned.csv', 'w') do |csv_output|
-  
-  File.open("data/PPR-2014-Dublin.csv").each do |line|
-    line.encode!("UTF-8", :invalid => :replace, :undef => :replace, :replace => "")
+
+["PPR-2013-Dublin.csv", "PPR-2014-Dublin.csv"].each do |data_file|
+
+  CSV.open("data/cleaned/#{data_file}", 'w') do |csv_output|
     
-    fields = CSV.parse_line(line)
-    fields[2] = extract_postcode(fields[1]) || fields[2]
-    fields[4] = remove_commas(fields[4])
-    csv_output << fields
+    File.open("data/#{data_file}").each do |line|
+      line.encode!("UTF-8", :invalid => :replace, :undef => :replace, :replace => "")
+      
+      fields = CSV.parse_line(line)
+      fields[2] = extract_postcode(fields[1]) || fields[2]
+      fields[4] = remove_commas(fields[4])
+      csv_output << fields
+    end
   end
 end
-
